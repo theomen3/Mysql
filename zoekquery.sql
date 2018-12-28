5d862201b51c6624beb885dcdfa9ab9f0f90c36a
nickzznl <45605610+nickzznl@users.noreply.github.com>
2018-12-28 22:44:23 +0100

    Update zoekquery.sql

--- zoekquery.sql
+++ zoekquery.sql
@@ -13,36 +13,38 @@ SELECT a.CustomerID, a.timeslot1, a.boksen, a.FirstName, b.FirstName FROM koppel
     OR a.preferredgender = 'beide')
     AND (b.preferredgender = a.gender
     OR b.preferredgender = 'beide')
-    AND (a.timeslot1 = b.timeslot1
-         OR a.timeslot2 = b.timeslot2
-         OR a.timeslot3 = b.timeslot3
-         OR a.timeslot4 = b.timeslot4
-         OR a.timeslot5 = b.timeslot5
-         OR a.timeslot6 = b.timeslot6
-         OR a.timeslot7 = b.timeslot7
-         OR a.timeslot8 = b.timeslot8
-         OR a.timeslot9 = b.timeslot9
-         OR a.timeslot10 = b.timeslot10
-         OR a.timeslot11 = b.timeslot11
-         OR a.timeslot12 = b.timeslot12
-         OR a.timeslot13 = b.timeslot13
-         OR a.timeslot14 = b.timeslot14
-         OR a.timeslot15 = b.timeslot15
-         OR a.timeslot16 = b.timeslot16
-         OR a.timeslot17 = b.timeslot17
-         OR a.timeslot18 = b.timeslot18
-         OR a.timeslot19 = b.timeslot19
-         OR a.timeslot20 = b.timeslot20
-         OR a.timeslot21 = b.timeslot21)
+    AND (a.timeslot1 = 1 AND b.timeslot1 = 1
+         OR a.timeslot2 = 1 AND b.timeslot2 = 1
+         OR a.timeslot3 = 1 AND b.timeslot3 = 1
+         OR a.timeslot4 = 1 AND b.timeslot4 = 1
+         OR a.timeslot5 = 1 AND b.timeslot5 = 1
+         OR a.timeslot6 = 1 AND b.timeslot6 = 1
+         OR a.timeslot7 = 1 AND b.timeslot7 = 1
+         OR a.timeslot8 = 1 AND b.timeslot8 = 1
+         OR a.timeslot9 = 1 AND b.timeslot9 = 1
+         OR a.timeslot10 = 1 AND b.timeslot10 = 1
+         OR a.timeslot11 = 1 AND b.timeslot11 = 1
+         OR a.timeslot12 = 1 AND b.timeslot12 = 1
+         OR a.timeslot13 = 1 AND b.timeslot13 = 1
+         OR a.timeslot14 = 1 AND b.timeslot14 = 1
+         OR a.timeslot15 = 1 AND b.timeslot15 = 1
+         OR a.timeslot16 = 1 AND b.timeslot16 = 1
+         OR a.timeslot17 = 1 AND b.timeslot17 = 1
+         OR a.timeslot18 = 1 AND b.timeslot18 = 1
+         OR a.timeslot19 = 1 AND b.timeslot19 = 1
+         OR a.timeslot20 = 1 AND b.timeslot20 = 1
+         OR a.timeslot21 = 1 AND b.timeslot21 = 1)
      AND (a.boksen = b.boksen 
-          OR a.fitness = b.fitness 
-          OR a.hardlopen = b.hardlopen 
-          OR a.tennis = b.tennis 
-          OR a.squash = b.squash 
-          OR a.wandelen = b.wandelen 
-          OR a.wielrennen = b.wielrennen 
-          OR a.zwemmen = b.zwemmen);
-          
+          OR a.fitness = 1 AND b.fitness = 1
+          OR a.hardlopen = 1 AND b.hardlopen = 1
+          OR a.tennis = 1 AND b.tennis = 1
+          OR a.squash = 1 AND b.squash = 1
+          OR a.wandelen = 1 AND b.wandelen = 1
+          OR a.wielrennen = 1 AND b.wielrennen = 1 
+          OR a.zwemmen = 1 AND b.zwemmen = 1);
+      
+      
+-------------------------------------------------------------------------------------------------------------------------      
           
 DROP VIEW IF EXISTS `koppel`;
 CREATE VIEW koppel AS
