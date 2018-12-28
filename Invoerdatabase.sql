baa2ba4245fd1ec4a6576e12cd18c88bc0c8e746
nickzznl <45605610+nickzznl@users.noreply.github.com>
2018-12-28 22:22:25 +0100

    Update Invoerdatabase.sql

--- Invoerdatabase.sql
+++ Invoerdatabase.sql
@@ -34,3 +34,42 @@ VALUES (3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
         
 INSERT INTO `timeslot`
 VALUES (4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+
+----------------------------------------------------------------------------------------------------
+
+INSERT INTO `customer` 
+VALUES (1, 'Frans', 'Jadus', '0648470015', 'man',  '1993-07-09', 'man');
+        
+INSERT INTO `customer`
+VALUES (2, 'Jurger', 'Papjes', '0612345678', 'man', '1980-08-12', 'vrouw');
+        
+INSERT INTO `customer`
+VALUES (3, 'Joost', 'Dinges', '0612545678', 'man', '1960-05-11', 'man');
+        
+INSERT INTO `customer`
+VALUES (4, 'Stijn', 'Ookiets', '0612545678', 'man', '1988-07-11', 'man');
+
+INSERT INTO `interest`
+VALUES (1, 1);
+        
+INSERT INTO `interest`
+VALUES (2, 1);
+        
+INSERT INTO `interest`
+VALUES (3, 0);
+        
+INSERT INTO `interest`
+VALUES (4, 0);
+        
+
+INSERT INTO `timeslot`
+VALUES (1, 1);
+        
+INSERT INTO `timeslot`
+VALUES (2, 1);
+        
+INSERT INTO `timeslot`
+VALUES (3, 0);
+        
+INSERT INTO `timeslot`
+VALUES (4, 0);
