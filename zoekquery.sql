cb45f39506d7ce20144add3327ec1e003fef55b8
nickzznl <45605610+nickzznl@users.noreply.github.com>
2018-12-28 21:21:03 +0100

    Update zoekquery.sql

--- zoekquery.sql
+++ zoekquery.sql
@@ -7,9 +7,7 @@ JOIN timeslot ON customer.CustomerID = timeslot.CustomerIDtimeslot
 
 SELECT a.CustomerID, a.timeslot, a.interest, a.FirstName, b.FirstName FROM koppel a
   JOIN koppel b
-    ON a.interest = b.interest
-    AND a.timeslot = b.timeslot
-    AND a.customerID <> b.CustomerID
+    ON a.customerID <> b.CustomerID
     AND (a.preferredgender = b.gender
     OR a.preferredgender = 'beide')
     AND (b.preferredgender = a.gender
